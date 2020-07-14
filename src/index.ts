8bd824e7cb3ba24b2ac5c3a3169b93105e9c5857
Airton Santos <airton.santos3.1415@gmail.com>
2020-07-14 10:51:15 -0300

    init a new project Test

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { GraphQLServer } from 'graphql-yoga' 
+// const { schema } = require('../prisma/schema.prisma')
+import { schema } from '../prisma/schema'
+
+
+const server = new GraphQLServer({
+  schema,
+
+})
